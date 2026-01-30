5b74911f61f0be60d21ea3a5d878d711726c8136
Sudhanshu Swami <91284690+Sudhanshu4909@users.noreply.github.com>
2026-01-30 20:36:38 +0530

    Update api.js

--- Frontend/content-moderation-dashboard/src/utils/api.js
+++ Frontend/content-moderation-dashboard/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dev-api.bigshorts.social/api/app/auth",
+  baseURL: "",
 });
 
 api.interceptors.request.use((config) => {
